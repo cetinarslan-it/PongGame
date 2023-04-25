36e08af8fadceb569365ab7cdb06cdb8af780692
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2023-04-25 23:47:20 +0200

    Refactor code

--- public/game.js
+++ public/game.js
@@ -3,31 +3,41 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const ball = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
-  dx: 3,
-  dy: -3,
+  x: 400,
+  y: 300,
   radius: 15,
 };
 
-let player1Score = 0;
-let player2Score = 0;
+// Set up player paddles
+let player1 = null;
+let player2 = null;
+let playerNo = 0;
 
 // Send paddle position updates to server on arrow key input
 document.addEventListener("keydown", (event) => {
   if (event.code === "ArrowUp") {
-    socket.emit("paddlePositionUpdate", { paddle: "left", direction: "up" });
+    event.preventDefault();
+
+    socket.emit("paddlePositionUpdate", {
+      playerNo: playerNo,
+      direction: "up",
+    });
   } else if (event.code === "ArrowDown") {
-    socket.emit("paddlePositionUpdate", { paddle: "left", direction: "down" });
-  } else if (event.code === "KeyW") {
-    socket.emit("paddlePositionUpdate", { paddle: "right", direction: "up" });
-  } else if (event.code === "KeyS") {
-    socket.emit("paddlePositionUpdate", { paddle: "right", direction: "down" });
+    event.preventDefault();
+
+    socket.emit("paddlePositionUpdate", {
+      playerNo: playerNo,
+      direction: "down",
+    });
   }
+
+  draw();
 });
 
 // Draw game objects on canvas
 function draw() {
+  if (!player1 || !player2) return;
+
   // Clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -50,7 +60,7 @@ function draw() {
   // Draw player1 paddle
   ctx.beginPath();
   ctx.rect(0, player1.y, player1.width, player1.height);
-  ctx.fillStyle = "black";
+  ctx.fillStyle = playerNo == 1 ? "blue" : "red";
   ctx.fill();
   ctx.closePath();
 
@@ -62,114 +72,41 @@ function draw() {
     player2.width,
     player2.height
   );
-  ctx.fillStyle = "black";
+  ctx.fillStyle = playerNo == 2 ? "blue" : "red";
   ctx.fill();
   ctx.closePath();
 
   // Draw score
   ctx.font = "22px Arial";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText(`Player 1: ${player1Score}`, 8, 30);
-  ctx.fillText(`Player 2: ${player2Score}`, canvas.width - 115, 30);
-}
-
-// Update game objects and check for collisions
-function update() {
-  // Move ball
-  ball.x += ball.dx;
-  ball.y += ball.dy;
-
-  // Check for collisions with walls
-  if (
-    ball.y + ball.dy < ball.radius ||
-    ball.y + ball.dy > canvas.height - ball.radius
-  ) {
-    ball.dy = -ball.dy;
-  }
-
-  // Check for collisions with player paddles
-  if (
-    ball.x - ball.radius < player1.width &&
-    ball.y > player1.y &&
-    ball.y < player1.y + player1.height
-  ) {
-    ball.dx = -ball.dx;
-  } else if (
-    ball.x + ball.radius > canvas.width - player2.width &&
-    ball.y > player2.y &&
-    ball.y < player2.y + player2.height
-  ) {
-    ball.dx = -ball.dx;
-  }
-
-  // Check if ball goes offscreen
-  if (ball.x - ball.radius < 0) {
-    player2Score++;
-    socket.emit("scoreUpdate", { player: 2, score: player2Score });
-    resetBall();
-  } else if (ball.x + ball.radius > canvas.width) {
-    player1Score++;
-    socket.emit("scoreUpdate", { player: 1, score: player1Score });
-    resetBall();
-  }
-}
-
-// Reset ball to center of canvas
-function resetBall() {
-  ball.x = canvas.width / 2;
-  ball.y = canvas.height / 2;
-  ball.dx = -ball.dx;
-  ball.dy = Math.floor(Math.random() * 6) - 3;
-}
-
-// Update player paddles based on server state
-function updatePaddles(paddles) {
-  player1.y = paddles.left;
-  player2.y = paddles.right;
-}
-
-// Update player scores based on server state
-function updateScores(scores) {
-  player1Score = scores[1];
-  player2Score = scores[2];
-}
-
-// Start game loop
-function startGame() {
-  setInterval(() => {
-    update();
-    draw();
-  }, 1000);
+  ctx.fillText(`Player 1: ${player1.score}`, 8, 30);
+  ctx.fillText(`Player 2: ${player2.score}`, canvas.width - 115, 30);
 }
 
 // Start game when server signals that both players have connected
-socket.on("startGame", () => {
-  startGame();
+socket.on("setPlayerNo", (data) => {
+  playerNo = data.playerNo;
 });
 
-// Receive updated paddle positions from server
-socket.on("paddlePositionUpdate", (data) => {
-  const paddle = data.paddle;
-  const direction = data.direction;
-  if (paddle === "left" && direction === "up") {
-    player1.y -= 7;
-  } else if (paddle === "left" && direction === "down") {
-    player1.y += 7;
-  } else if (paddle === "right" && direction === "up") {
-    player2.y -= 7;
-  } else if (paddle === "right" && direction === "down") {
-    player2.y += 7;
-  }
+socket.on("ballUpdate", (data) => {
+  ball.x = data.x;
+  ball.y = data.y;
+  draw();
+});
+
+socket.on("playerPositionUpdate", (data) => {
+  player1 = data.player1;
+  player2 = data.player2;
+  draw();
 });
 
 // Receive updated scores from server
-socket.on("scoreUpdate", (data) => {
-  updateScores(data);
+socket.on("scoreUpdate", (scores) => {
+  player1.score = scores.player1Score;
+  player2.score = scores.player2Score;
+
+  draw();
 });
 
-// Set up player paddles
-const player1 = { width: 20, height: 150, y: canvas.height / 2 - 150 / 2 };
-const player2 = { width: 20, height: 150, y: canvas.height / 2 - 150 / 2 };
-
 // Draw initial game state
-draw();
+//draw();
--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const socketIo = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
+let connections = [];
+
+// Set up player paddles
+const player1 = { width: 20, height: 150, y: 300 - 150 / 2, score: 0 };
+const player2 = { width: 20, height: 150, y: 300 - 150 / 2, score: 0 };
+
+const ball = {
+  x: 400,
+  y: 300,
+  dx: 1,
+  dy: -1,
+  radius: 15,
+};
+
 // Set up static file serving for client-side files
 app.use(express.static(__dirname + "/public"));
 
@@ -22,23 +36,105 @@ server.listen(PORT, () => {
 });
 
 io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
+  connections.push(socket);
+
+  console.log("Client connected:", socket.id, " length:", connections.length);
 
   // Handled paddle position updates from client
   socket.on("paddlePositionUpdate", (data) => {
-    // Broadcasted paddle position update to all clients except sender
-    socket.broadcast.emit("paddlePositionUpdate", data);
-  });
+    console.log("Command is " + data.direction);
 
-  // Handled score updates from client
-  socket.on("scoreUpdate", (data) => {
-    // Broadcasted score update to all clients except sender
-    socket.broadcast.emit("scoreUpdate", data);
+    var player = data.playerNo == 1 ? player1 : player2;
+
+    if (data.direction == "up") player.y -= 5;
+    else player.y += 5;
+
+    sendToAll("playerPositionUpdate", { player1: player1, player2: player2 });
   });
 
   // Handled disconnect event from client
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
+    connections.splice(connections.indexOf(socket), 1);
   });
+
+  //Start game if there are 2 clients
+  if (connections.length == 2) {
+    connections[0].emit("setPlayerNo", { playerNo: 1 });
+    connections[1].emit("setPlayerNo", { playerNo: 2 });
+
+    sendToAll("playerPositionUpdate", { player1: player1, player2: player2 });
+
+    resetBall();
+
+    setInterval(() => {
+      manageGameRoom();
+    }, 10);
+  }
 });
 
+function manageGameRoom() {
+  moveBall();
+
+  // Check for collisions with walls
+  if (ball.y + ball.dy < ball.radius || ball.y + ball.dy > 600 - ball.radius) {
+    ball.dy = -ball.dy;
+  }
+
+  // Check for collisions with player paddles
+  if (
+    ball.x - ball.radius < player1.width &&
+    ball.y > player1.y &&
+    ball.y < player1.y + player1.height
+  ) {
+    ball.dx = -ball.dx;
+  } else if (
+    ball.x + ball.radius > 800 - player2.width &&
+    ball.y > player2.y &&
+    ball.y < player2.y + player2.height
+  ) {
+    ball.dx = -ball.dx;
+  }
+
+  // Check if ball goes offscreen
+  if (ball.x - ball.radius < 0) {
+    player2.score++;
+    sendToAll("scoreUpdate", {
+      player1Score: player1.score,
+      player2Score: player2.score,
+    });
+    resetBall();
+  } else if (ball.x + ball.radius > 800) {
+    player1.score++;
+    sendToAll("scoreUpdate", {
+      player1Score: player1.score,
+      player2Score: player2.score,
+    });
+    resetBall();
+  }
+
+  sendToAll("ballUpdate", { x: ball.x, y: ball.y });
+}
+
+function sendToAll(command, data) {
+  io.sockets.emit(command, data);
+}
+
+function moveBall() {
+  // Move ball
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+}
+
+function resetBall() {
+  ball.x = 400;
+  ball.y = 300;
+
+  console.log(Math.random());
+
+  if (Math.random() > 0.5) ball.dx = -1;
+  else ball.dx = 1;
+
+  if (Math.random() > 0.5) ball.dy = -1;
+  else ball.dy = 1;
+}
